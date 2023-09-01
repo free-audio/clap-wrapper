b046b7e70c1ccdcb8013f76c186eb516a5845a5e
Paul <baconpaul@users.noreply.github.com>
2023-09-01 14:03:55 -0400

    Make the CMake more library-like still (#70)
    * restructuring code segments to libraries
    
    * WIP: entrypoint symbols missing
    
    * continue the lib everything push. This is the compact form building
    on macOS so far only
    
    * Windows Single File defaults to True
    
    * And now Linux too
    
    * We no longer need nomainwq
    
    ---------
    
    Co-authored-by: defiantnerd <tim@defiantnerd.com>

---
+++ src/wrapasvst3_export_entry.cpp
@@ -0,0 +1,7 @@
+
+#include "public.sdk/source/main/pluginfactory.h"
+
+SMTG_EXPORT_SYMBOL Steinberg::IPluginFactory* PLUGIN_API GetPluginFactory() {
+   extern Steinberg::IPluginFactory *GetPluginFactoryEntryPoint();
+   return GetPluginFactoryEntryPoint();
+}
