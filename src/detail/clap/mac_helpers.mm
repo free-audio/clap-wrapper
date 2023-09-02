81464bdc9ea276f3d9282c5d0d9512245a5787f9
Paul <baconpaul@users.noreply.github.com>
2023-09-02 16:09:37 -0400

    Add an 'in-bundle' self contained load macOS option (#88)
    macOS bundles support a PlugIn directory (Contents/PlugIns) which
    contain subordinate plug-ins to be loaded. This commit does two things
    
    1. Adds a MACOS_EMBEDDED_CLAP_LOCATION option to target_add_auv2/vst3_wrapper
       which if set creates the Contents/Plugins in the bundle and copys the
       plugin there; and
    
    2. Modifies the mac system paths to look up the current running bundle
       and then use CFBundleCopyBuiltInPlugInsURL to get this location and
       pre-pend it to the clap load search path.
    
    Result: Fully self contained VST3 and AUV2.

--- src/detail/clap/mac_helpers.mm
+++ src/detail/clap/mac_helpers.mm
@@ -10,6 +10,7 @@
 */
 
 #include <vector>
+#include <dlfcn.h>
 
 // No need to ifdef this - it is mac only
 #include <ghc/filesystem.hpp>
@@ -17,10 +18,67 @@ namespace fs = ghc::filesystem;
 
 #include <Foundation/Foundation.h>
 
+
 namespace Clap
 {
+  fs::path sharedLibraryBundlePath()
+  {
+    Dl_info info;
+    if (!dladdr(reinterpret_cast<const void *>(&sharedLibraryBundlePath), &info) ||
+        !info.dli_fname[0])
+    {
+      // If dladdr(3) returns zero, dlerror(3) won't know why either
+      return {};
+    }
+    try
+    {
+      auto res = fs::path{info.dli_fname};
+      res = res.parent_path().parent_path(); // this might throw if not in bundle so catch
+      if (res.filename().u8string() == "Contents" && res.has_parent_path())
+      {
+        // We are properly situated in a bundle in either a MacOS ir an iOS location
+        return res.parent_path();
+      }
+    }
+    catch(const fs::filesystem_error &)
+    {
+      // oh well
+    }
+    return {};
+  }
+
     std::vector<fs::path> getMacCLAPSearchPaths() {
-       auto res = std::vector<fs::path>();
+      auto res = std::vector<fs::path>();
+
+      auto bundlePath = sharedLibraryBundlePath();
+      if (!bundlePath.empty())
+      {
+        std::string name = bundlePath.u8string();
+        CFURLRef bundleUrl = CFURLCreateFromFileSystemRepresentation (0,
+                                                                   (const unsigned char*)name.c_str (),
+                                                                   name.size(), true);
+       if (bundleUrl)
+       {
+         auto pluginBundle = CFBundleCreate (0, bundleUrl);
+         CFRelease (bundleUrl);
+
+         if (pluginBundle)
+         {
+           auto pluginFoldersUrl = CFBundleCopyBuiltInPlugInsURL(pluginBundle);
+
+           if (pluginFoldersUrl)
+           {
+             // Remember CFURL and NSURL are toll free bridged
+             auto *ns = (NSURL *)pluginFoldersUrl;
+             auto pp = fs::path{[ns fileSystemRepresentation]};
+             res.push_back(pp);
+             CFRelease(pluginFoldersUrl);
+           }
+           CFRelease(pluginBundle);
+         }
+       }
+      }
+
        auto *fileManager = [NSFileManager defaultManager];
        auto *userLibURLs = [fileManager URLsForDirectory:NSLibraryDirectory
                                                inDomains:NSUserDomainMask];
