6a024bd8b47e5e6cafda7e2ad94409ec03e515c4
Alexander Droste <alexander.droste@protonmail.com>
2023-09-18 17:07:00 +0200

    fix wrap mask

--- src/detail/standalone/standalone_details.h
+++ src/detail/standalone/standalone_details.h
@@ -21,7 +21,7 @@ class fixedqueue
   inline void push(const T* val)
   {
     _elements[_head] = *val;
-    _head = (_head + 1) & Q;
+    _head = (_head + 1) & _wrapMask;
   }
   inline bool pop(T& out)
   {
@@ -30,7 +30,7 @@ class fixedqueue
       return false;
     }
     out = _elements[_tail];
-    _tail = (_tail + 1) & Q;
+    _tail = (_tail + 1) & _wrapMask;
     return true;
   }
 
@@ -39,6 +39,7 @@ class fixedqueue
   std::atomic_uint32_t _head = 0u;
   std::atomic_uint32_t _tail = 0u;
 
+  uint32_t _wrapMask = Q - 1;
   static_assert((Q & (Q - 1)) == 0, "Q needs to be a multiple of 2");
 };
 }  // namespace Clap::Standalone
