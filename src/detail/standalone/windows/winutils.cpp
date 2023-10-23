c2925545b6633d84d924e616b4788521d5766861
mthierman <mthierman@gmail.com>
2023-10-23 16:27:20 -0600

    Initial DPI scale for win standalone

--- src/detail/standalone/windows/winutils.cpp
+++ src/detail/standalone/windows/winutils.cpp
@@ -8,116 +8,51 @@
 #include "../../clap/fsutil.h"
 
 #include "winutils.h"
+#include "helper.h"
 
 namespace freeaudio::clap_wrapper::standalone::windows
 {
-// A very basic win32 window class
-class Window
+struct Window
 {
- public:
   Window();
   ~Window();
 
-  HWND get_hwnd();
-
- private:
-  HWND m_hwnd;
   static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+  virtual int _OnClose(HWND, UINT, WPARAM, LPARAM);
+  virtual int _OnDestroy(HWND, UINT, WPARAM, LPARAM);
+  virtual int _OnDpiChanged(HWND, UINT, WPARAM, LPARAM);
+  virtual int _OnKeyDown(HWND, UINT, WPARAM, LPARAM);
+  virtual int _OnSize(HWND, UINT, WPARAM, LPARAM);
+
+  HWND m_hwnd;
+  bool fullscreen;
 };
-
-// Get window instance in our WndProc and set the m_hwnd member variable on creation
-// also hide the console since we aren't a real Win32 application
-template <class T, class U, HWND(U::*m_hwnd)>
-T* InstanceFromWndProc(HWND hwnd, UINT umsg, LPARAM lparam)
-{
-  T* pInstance;
-
-  if (umsg == WM_NCCREATE)
-  {
-    LPCREATESTRUCT pCreateStruct{reinterpret_cast<LPCREATESTRUCT>(lparam)};
-    pInstance = reinterpret_cast<T*>(pCreateStruct->lpCreateParams);
-    ::SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pInstance));
-    pInstance->*m_hwnd = hwnd;
-    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-  }
-
-  else
-    pInstance = reinterpret_cast<T*>(::GetWindowLongPtrW(hwnd, GWLP_USERDATA));
-
-  return pInstance;
-}
-
-std::string narrow(std::wstring in)
-{
-  if (!in.empty())
-  {
-    auto inSize{static_cast<int>(in.size())};
-
-    auto outSize{::WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS, in.data(),
-                                       inSize, nullptr, 0, nullptr, nullptr)};
-
-    if (outSize > 0)
-    {
-      std::string out;
-      out.resize(static_cast<size_t>(outSize));
-
-      if (::WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS, in.data(), inSize,
-                                out.data(), outSize, nullptr, nullptr) > 0)
-        return out;
-    }
-  }
-
-  return {};
-}
-
-std::wstring widen(std::string in)
-{
-  if (!in.empty())
-  {
-    auto inSize{static_cast<int>(in.size())};
-
-    auto outSize{::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, in.data(), inSize, nullptr, 0)};
-
-    if (outSize > 0)
-    {
-      std::wstring out;
-      out.resize(static_cast<size_t>(outSize));
-
-      if (::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, in.data(), inSize, out.data(), outSize) >
-          0)
-        return out;
-    }
-  }
-
-  return {};
-}
 
 Window::Window()
 {
   ::SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-  std::wstring clapName{widen(WIN32_TITLE)};
+  std::wstring clapName{helper::widen(WIN32_TITLE)};
+  std::wstring randomName{helper::randomize(clapName)};
 
-  WNDCLASSEXW wcex{
-      sizeof(WNDCLASSEX),
-      CS_HREDRAW | CS_VREDRAW,
-      Window::WndProc,
-      0,
-      0,
-      ::GetModuleHandleW(nullptr),
-      (HICON)::LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
-                          LR_DEFAULTCOLOR | LR_DEFAULTSIZE | LR_SHARED),
-      (HCURSOR)::LoadImageW(nullptr, (LPCWSTR)IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED),
-      (HBRUSH)::GetStockObject(BLACK_BRUSH),
-      clapName.c_str(),
-      clapName.c_str(),
-      (HICON)::LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
-                          LR_DEFAULTCOLOR | LR_DEFAULTSIZE | LR_SHARED),
-  };
+  WNDCLASSEXW wcex{sizeof(WNDCLASSEX)};
+  wcex.lpszClassName = randomName.c_str();
+  wcex.lpszMenuName = randomName.c_str();
+  wcex.lpfnWndProc = Window::WndProc;
+  wcex.style = 0;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = ::GetModuleHandleW(nullptr);
+  wcex.hbrBackground = reinterpret_cast<HBRUSH>(::GetStockObject(BLACK_BRUSH));
+  wcex.hCursor = (HCURSOR)::LoadImageW(nullptr, (LPCWSTR)IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
+  wcex.hIcon = (HICON)::LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
+                                   LR_DEFAULTCOLOR | LR_DEFAULTSIZE | LR_SHARED);
+  wcex.hIconSm = (HICON)::LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
+                                     LR_DEFAULTCOLOR | LR_DEFAULTSIZE | LR_SHARED);
 
   ::RegisterClassExW(&wcex);
 
-  ::CreateWindowExW(0, clapName.c_str(), clapName.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+  ::CreateWindowExW(0, randomName.c_str(), clapName.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
                     CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr,
                     ::GetModuleHandleW(nullptr), this);
 
@@ -128,94 +63,114 @@ Window::~Window()
 {
 }
 
-HWND Window::get_hwnd()
+LRESULT CALLBACK Window::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-  return m_hwnd;
-}
+  Window* pWindow = helper::InstanceFromWndProc<Window, &Window::m_hwnd>(hWnd, uMsg, lParam);
 
-LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-  Window* window = InstanceFromWndProc<Window, Window, &Window::m_hwnd>(hwnd, msg, lparam);
-
-  if (window)
+  if (pWindow)
   {
-    switch (msg)
+    switch (uMsg)
     {
-      case WM_SIZE:
-      {
-        RECT r{0, 0, 0, 0};
-        ::GetClientRect(hwnd, &r);
-
-        auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
-
-        // Need to check if plugin is resizable, disable plugin gui resizing for now
-        if (plugin && plugin->_ext._gui &&
-            freeaudio::clap_wrapper::standalone::getStandaloneHost()->gui_can_resize())
-          plugin->_ext._gui->set_size(plugin->_plugin, (r.right - r.left), (r.bottom - r.top));
-
-        return 0;
-      }
-
       case WM_CLOSE:
-      {
-        auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
-
-        if (plugin && plugin->_ext._gui)
-        {
-          plugin->_ext._gui->hide(plugin->_plugin);
-          plugin->_ext._gui->destroy(plugin->_plugin);
-        }
-
-        ::DestroyWindow(hwnd);
-
-        return 0;
-      }
-
+        return pWindow->_OnClose(hWnd, uMsg, wParam, lParam);
       case WM_DESTROY:
-      {
-        freeaudio::clap_wrapper::standalone::mainFinish();
-
-        ::PostQuitMessage(0);
-
-        return 0;
-      }
-
+        return pWindow->_OnDestroy(hWnd, uMsg, wParam, lParam);
       case WM_DPICHANGED:
-      {
-        // Since scaling is not fully implemented, get plugin dimensions and size window to initial value
-
-        auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
-
-        uint32_t w;
-        uint32_t h;
-        plugin->_ext._gui->get_size(plugin->_plugin, &w, &h);
-
-        RECT r{0, 0, 0, 0};
-        r.right = w;
-        r.bottom = h;
-
-        auto dpi{::GetDpiForWindow(hwnd)};
-
-        // auto scaleFactor{static_cast<float>(dpi) / static_cast<float>(USER_DEFAULT_SCREEN_DPI)};
-
-        // LOG << "dpi: " << dpi << std::endl;
-        // LOG << "scaleFactor: " << scaleFactor << std::endl;
-
-        ::AdjustWindowRectExForDpi(&r, WS_OVERLAPPEDWINDOW, 0, 0, dpi);
-        ::SetWindowPos(hwnd, nullptr, 0, 0, (r.right - r.left), (r.bottom - r.top), SWP_NOMOVE);
-
-        // Resize the window to suggested rectangle from system
-        // auto suggestedRect{(RECT*)lparam};
-        // ::SetWindowPos(hwnd, nullptr, suggestedRect->left, suggestedRect->top,
-        //              (suggestedRect->right - suggestedRect->left),
-        //              (suggestedRect->bottom - suggestedRect->top), SWP_NOZORDER | SWP_NOACTIVATE);
-
-        return 0;
-      }
+        return pWindow->_OnDpiChanged(hWnd, uMsg, wParam, lParam);
+      case WM_KEYDOWN:
+        return pWindow->_OnKeyDown(hWnd, uMsg, wParam, lParam);
+      case WM_SIZE:
+        return pWindow->_OnSize(hWnd, uMsg, wParam, lParam);
     }
   }
 
-  return ::DefWindowProcW(hwnd, msg, wparam, lparam);
+  return ::DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
+
+int Window::_OnClose(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
+
+  if (plugin && plugin->_ext._gui)
+  {
+    plugin->_ext._gui->hide(plugin->_plugin);
+    plugin->_ext._gui->destroy(plugin->_plugin);
+  }
+
+  ::DestroyWindow(m_hwnd);
+
+  return 0;
+}
+
+int Window::_OnDestroy(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  freeaudio::clap_wrapper::standalone::mainFinish();
+  ::PostQuitMessage(0);
+
+  return 0;
+}
+
+int Window::_OnDpiChanged(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  // auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
+
+  // uint32_t w;
+  // uint32_t h;
+  // plugin->_ext._gui->get_size(plugin->_plugin, &w, &h);
+
+  // RECT r{0, 0, 0, 0};
+  // r.right = w;
+  // r.bottom = h;
+
+  // auto dpi{::GetDpiForWindow(hWnd)};
+
+  // auto scaleFactor{static_cast<float>(dpi) / static_cast<float>(USER_DEFAULT_SCREEN_DPI)};
+
+  // LOG << "dpi: " << dpi << std::endl;
+  // LOG << "scaleFactor: " << scaleFactor << std::endl;
+
+  // ::AdjustWindowRectExForDpi(&r, WS_OVERLAPPEDWINDOW, 0, 0, dpi);
+  // ::SetWindowPos(hWnd, nullptr, 0, 0, (r.right - r.left), (r.bottom - r.top), SWP_NOMOVE);
+
+  // Resize the window to suggested rectangle from system
+  // auto suggestedRect{(RECT*)lparam};
+  // ::SetWindowPos(hWnd, nullptr, suggestedRect->left, suggestedRect->top,
+  //              (suggestedRect->right - suggestedRect->left),
+  //              (suggestedRect->bottom - suggestedRect->top), SWP_NOZORDER | SWP_NOACTIVATE);
+
+  return 0;
+}
+
+int Window::_OnKeyDown(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch (wParam)
+  {
+    case VK_F11:
+    {
+      fullscreen = helper::fullscreen(m_hwnd);
+
+      break;
+    }
+
+    default:
+      return 0;
+  }
+
+  return 0;
+}
+
+int Window::_OnSize(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  // auto plugin{freeaudio::clap_wrapper::standalone::getMainPlugin()};
+
+  // if (plugin && plugin->_ext._gui)
+  // {
+  //   RECT r{0, 0, 0, 0};
+  //   ::GetClientRect(hWnd, &r);
+  //   plugin->_ext._gui->set_size(plugin->_plugin, (r.right - r.left), (r.bottom - r.top));
+  // }
+
+  return 0;
 }
 
 void Win32Gui::initialize(freeaudio::clap_wrapper::standalone::StandaloneHost* sah)
@@ -232,34 +187,37 @@ void Win32Gui::run()
 {
   if (plugin->_ext._gui)
   {
+    Window window;
+    RECT r{0, 0, 0, 0};
+
+    auto dpi{::GetDpiForWindow(window.m_hwnd)};
+    auto scaleFactor{static_cast<float>(dpi) / static_cast<float>(USER_DEFAULT_SCREEN_DPI)};
+
     auto ui{plugin->_ext._gui};
     auto p{plugin->_plugin};
 
     if (!ui->is_api_supported(p, CLAP_WINDOW_API_WIN32, false)) LOG << "NO WIN32 " << std::endl;
 
     ui->create(p, CLAP_WINDOW_API_WIN32, false);
-    ui->set_scale(p, 1);
+    ui->set_scale(p, scaleFactor);
 
     uint32_t w;
     uint32_t h;
     ui->get_size(p, &w, &h);
 
-    Window window;
-    RECT r{0, 0, 0, 0};
-    r.right = w;
-    r.bottom = h;
-
-    auto dpi{::GetDpiForWindow(window.get_hwnd())};
-    // auto scaleFactor{static_cast<float>(dpi) / static_cast<float>(USER_DEFAULT_SCREEN_DPI)};
-
-    ::AdjustWindowRectExForDpi(&r, WS_OVERLAPPEDWINDOW, 0, 0, dpi);
-    ::SetWindowPos(window.get_hwnd(), nullptr, 0, 0, (r.right - r.left), (r.bottom - r.top), SWP_NOMOVE);
+    ui->set_size(p, (w * scaleFactor), (h * scaleFactor));
 
     clap_window win;
     win.api = CLAP_WINDOW_API_WIN32;
-    win.win32 = (void*)window.get_hwnd();
+    win.win32 = (void*)window.m_hwnd;
     ui->set_parent(p, &win);
     ui->show(p);
+
+    r.right = w * scaleFactor;
+    r.bottom = h * scaleFactor;
+
+    ::AdjustWindowRectExForDpi(&r, WS_OVERLAPPEDWINDOW, 0, 0, dpi);
+    ::SetWindowPos(window.m_hwnd, nullptr, 0, 0, (r.right - r.left), (r.bottom - r.top), SWP_NOMOVE);
   }
 
   MSG msg;
--- src/detail/standalone/windows/winutils.h
+++ src/detail/standalone/windows/winutils.h
@@ -3,12 +3,10 @@
 #include <clap_proxy.h>
 #include "detail/standalone/standalone_host.h"
 
-struct _Win32Application;
 namespace freeaudio::clap_wrapper::standalone::windows
 {
 struct Win32Gui
 {
-  _Win32Application *app{nullptr};
   std::shared_ptr<Clap::Plugin> plugin;
 
   void initialize(freeaudio::clap_wrapper::standalone::StandaloneHost *);
