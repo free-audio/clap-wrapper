538a7dfcc6fc2af67dcb11041a1dde91ae09be0f
mthierman <mthierman@gmail.com>
2026-01-21 10:54:03 -0700

    Windows Standalone: Fix clobbering loaded audio API

--- src/detail/standalone/windows/windows_standalone.cpp
+++ src/detail/standalone/windows/windows_standalone.cpp
@@ -1059,10 +1059,10 @@ Plugin::Plugin(const clap_plugin_entry* entry, int argc, char** argv)
 
   if (loadSettings())
   {
-    initializeAudio();
+    initializeAudio(sah->audioApi);
     menu.item[1].fState = sah->audioInputUsed ? MFS_UNCHECKED : MFS_CHECKED;
     SetMenuItemInfoW(getSystemMenu(hwnd.get()), 1, FALSE, &menu.item[1]);
-    sah->setAudioApi(sah->audioApi);
+    // sah->setAudioApi(sah->audioApi);
   }
   else
   {
